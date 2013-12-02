578431dbdfc46d5953ee149015a935bfe028eedf
Zhaph - Ben Duguid <ben@doodle.co.uk>
2013-12-02 00:15:49 +0000

    First Pass at Starting a Race.

---
+++ LapTimes/Areas/ManageRaces/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using LapTimes.Models;
+
+namespace LapTimes.Areas.ManageRaces.Controllers
+{
+    public class HomeController : Controller
+    {
+      private LapTimesContext db = new LapTimesContext();
+
+      private int _numberOfLanes = int.Parse(WebConfigurationManager.AppSettings["RaceLanes"]);
+      
+        //
+        // GET: /ManageRaces/Home/
+
+        public ActionResult Index()
+        {
+          ViewBag.CarId = new SelectList(db.Cars.OrderBy(c => c.CarId), "CarId", "Name");
+          ViewBag.RacerId = new SelectList(db.Racers.Where(r => r.IsWaitingForRace).OrderBy(c => c.Name), "RacerId",
+                                           "Name");
+          ViewBag.NumberOfLanes = _numberOfLanes;
+
+          var currentRace = db.Races.Where(r => !r.IsComplete).SingleOrDefault();
+
+          return View(currentRace);
+        }
+
+      [HttpPost]
+      public ActionResult Index(Race newRace)
+      {
+        if (ModelState.IsValid)
+        {
+          
+        }
+
+        ViewBag.CarId = new SelectList(db.Cars.OrderBy(c => c.CarId), "CarId", "Name");
+        ViewBag.RacerId = new SelectList(db.Racers.Where(r => r.IsWaitingForRace).OrderBy(c => c.Name), "RacerId",
+                                         "Name");
+
+        return View(newRace);
+      }
+
+    }
+}
