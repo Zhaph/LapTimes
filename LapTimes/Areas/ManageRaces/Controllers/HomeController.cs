071575d6faca7e186f05e23a975183a28030fca8
Zhaph - Ben Duguid <ben@doodle.co.uk>
2014-12-01 02:18:13 +0000

    Reworked to return class names along with the Driver names in lists as well to aid in identifying racers.

--- LapTimes/Areas/ManageRaces/Controllers/HomeController.cs
+++ LapTimes/Areas/ManageRaces/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Data;
+using System.Data.Entity;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -19,42 +21,42 @@ namespace LapTimes.Areas.ManageRaces.Controllers
         _repo = repo;
       }
 
-      private int _numberOfLanes = int.Parse(WebConfigurationManager.AppSettings["RaceLanes"]);
+      private readonly int _numberOfLanes = int.Parse(WebConfigurationManager.AppSettings["RaceLanes"]);
       
-        //
-        // GET: /ManageRaces/Home/
+      //
+      // GET: /ManageRaces/Home/
 
-        public ActionResult Index()
+      public ActionResult Index()
+      {
+        ViewBag.NumberOfLanes = _numberOfLanes;
+        ViewBag.CarId = new SelectList(_db.Cars.OrderBy(c => c.CarId), "CarId", "Name");
+        var racers = getFormattedRacers().ToList();
+        ViewBag.RacerId = new SelectList(racers, "RacerId", "Name");
+
+        if (racers.Any() && racers.Take(2).Count() == 2)
         {
-          ViewBag.NumberOfLanes = _numberOfLanes;
-          ViewBag.CarId = new SelectList(_db.Cars.OrderBy(c => c.CarId), "CarId", "Name");
-          var racers = _db.Racers.Where(r => r.IsWaitingForRace).OrderBy(c => c.Name).ToList();
-          ViewBag.RacerId = new SelectList(racers, "RacerId", "Name");
+          // There are at least two racers waiting
+          var currentRace = _repo.CurrentRace() ??
+                            new Race
+                              {
+                                RaceId = 0,
+                                Drivers =
+                                  new List<CurrentDriver>(_numberOfLanes)
+                              };
 
-          if (racers.Any() && racers.Take(2).Count() == 2)
+          if (currentRace.RaceId == 0)
           {
-            // There are at least two racers waiting
-            var currentRace = _repo.CurrentRace() ??
-                              new Race
-                                {
-                                  RaceId = 0,
-                                  Drivers =
-                                    new List<CurrentDriver>(_numberOfLanes)
-                                };
-
-            if (currentRace.RaceId == 0)
+            for (int lane = 0; lane < _numberOfLanes; lane++)
             {
-              for (int lane = 0; lane < _numberOfLanes; lane++)
-              {
-                currentRace.Drivers.Add(new CurrentDriver {Lane = lane + 1});
-              }
+              currentRace.Drivers.Add(new CurrentDriver {Lane = lane + 1});
             }
-
-            return View(currentRace);
           }
 
-          return RedirectToAction("Index", "Home", new {Area = "ManageRacers"});
+          return View(currentRace);
         }
+
+        return RedirectToAction("Index", "Home", new {Area = "ManageRacers"});
+      }
 
       [HttpPost]
       public ActionResult Index(Race newRace)
@@ -67,11 +69,28 @@ namespace LapTimes.Areas.ManageRaces.Controllers
         }
 
         ViewBag.CarId = new SelectList(_db.Cars.OrderBy(c => c.CarId), "CarId", "Name");
-        ViewBag.RacerId = new SelectList(_db.Racers.Where(r => r.IsWaitingForRace).OrderBy(c => c.Name), "RacerId",
-                                         "Name");
+
+        ViewBag.RacerId =
+          new SelectList(getFormattedRacers(),
+            "RacerId",
+            "Name");
 
         return View(newRace);
       }
 
+      private IEnumerable<FormattedRacer> getFormattedRacers()
+      {
+        return _db.Racers.Include(r => r.ClassName)
+          .Where(r => r.IsWaitingForRace)
+          .OrderBy(r => r.Name)
+          .AsEnumerable()
+          .Select(r => new FormattedRacer{RacerId = r.RacerId, Name = string.Format("{0} - {1}", r.Name, r.ClassName.Name)});
+      }
     }
+
+  public class FormattedRacer
+  {
+    public int RacerId { get; set; }
+    public string Name { get; set; }
+  }
 }
